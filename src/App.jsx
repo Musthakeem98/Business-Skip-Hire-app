4b38859b9acb4fed91b4c0dfabc5b099400816e8
musthakeen <musthakeen@kloudip.com>
2025-06-16 19:40:39 +0530

    time line is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="app-wrapper">
+      <TimelineStepper />
       <div className="header-section">
         <h1 className="skip-heading">Choose Your Skip Size</h1>
         <p className="skip-subheading">Select the skip size that best suits your needs</p>
