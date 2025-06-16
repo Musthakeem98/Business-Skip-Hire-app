9390e22224b8daae5ddbe0cfc6551244a48e4fb9
musthakeen <musthakeen@kloudip.com>
2025-06-16 19:06:45 +0530

    setup environment for project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TimelineStepper from './components/TimelineStepper/TimeLineStepper';
+import SkipGrid from './components/SkipGrid/SkipGrid';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+    </div>
+  );
+}
+
+export default App;
